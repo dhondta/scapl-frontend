4609dab030ed4c42fe259452dfabd3a8a7aa7c75
bahmadh <hussein_bahmad@hotmail.com>
2016-04-15 13:43:39 +0200

    requirements update

--- README.md
+++
@@ -1,5 +0,0 @@
-# scapl-frontend
-SCAPL frontend component.
-
-
-Written by A. D'Hondt and H. Bahmad
--- frontend/frontend/context_processors.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: UTF-8 -*-
-from django.conf import settings
-
-
-def project_name(request):
-    return {'PROJECT_NAME': settings.PROJECT_NAME, 'PROJECT_AUTHORS': settings.PROJECT_AUTHORS}
